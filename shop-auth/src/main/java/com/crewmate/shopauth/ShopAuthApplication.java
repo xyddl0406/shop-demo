ddc4a104867c35cd5b3822f3d33a9ca58f1ebd9f
xyddl0406 <xyddl0406@gmail.com>
2022-11-30 12:36:36 +0900

    자식프로젝트의 build.gradle삭제, 인증서버에서 DB연결설정 제외

--- shop-auth/src/main/java/com/crewmate/shopauth/ShopAuthApplication.java
+++ shop-auth/src/main/java/com/crewmate/shopauth/ShopAuthApplication.java
@@ -2,6 +2,7 @@ package com.crewmate.shopauth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
@@ -9,7 +10,7 @@ import com.crewmate.shopapicommon.properties.ApplicationProperties;
 import com.crewmate.shopapicommon.properties.ExternalApiProperties;
 
 @EnableFeignClients
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class}) //common-api에 의존되어있는 database 설정bean 이 동작하지 않게하는 제외설정
 @EnableConfigurationProperties({ApplicationProperties.class, ExternalApiProperties.class}) //application.yml에 설정한 변수를 javaconfig로 만들면 이렇게 선언해줘야 해당빈을 찾을 수 있다. 
 public class ShopAuthApplication {
 
